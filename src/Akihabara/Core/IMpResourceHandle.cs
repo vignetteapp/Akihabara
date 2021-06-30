922c0c517c642534e6428e919ef3051ba73f824f
Ayane Satomi <chinodesuuu@gmail.com>
2021-06-30 14:19:13 +0800

    Port C++ String STL
    Signed-off-by: Ayane Satomi <chinodesuuu@gmail.com>

---
+++ src/Akihabara/Core/IMpResourceHandle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Akihabara.Core
+{
+    public interface IMpResourceHandle : IDisposable
+    {
+        IntPtr MpPtr { get; }
+        
+        /// <summary>
+        ///  Relinquishes the ownership and release the resource, if necessary.
+        ///  Method should be called only if the underlying native API moves the ptr.
+        /// </summary>
+        /// <remarks>If the object is no longer used, call <see cref="Dispose"/> instead.</remarks>
+        void ReleaseMpResource();
+        
+        /// <summary>
+        ///  Relinquish ownership of the resource.
+        /// </summary>
+        void TransferOwnership();
+        
+        /// <summary>
+        ///  If the resource is owned.
+        /// </summary>
+        /// <returns>true if owned by the handler, false if not.</returns>
+        bool OwnsResource();
+    }
+}
---
+++ src/Akihabara/External/StdString.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Akihabara.Core;
+using Akihabara.Native.External;
+
+namespace Akihabara.External
+{
+    public class StdString : MpResourceHandle
+    {
+        public StdString(IntPtr ptr, bool isOwner = true) : base(ptr, isOwner)
+        {
+        }
+
+        public StdString(byte[] bytes) : base()
+        {
+            UnsafeNativeMethods.std_string__PKC_i(bytes, bytes.Length, out var ptr);
+            this.Ptr = ptr;
+        }
+
+        protected override void DeleteMpPtr()
+        {
+            UnsafeNativeMethods.std_string__delete(Ptr);
+        }
+
+        public void Swap(StdString str)
+        {
+            UnsafeNativeMethods.std_string__swap__Rstr(MpPtr, str.MpPtr);
+            GC.KeepAlive(this);
+        }
+    }
+}
