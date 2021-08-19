3825c333b1ac682c86b98a03f32ae3a117f1f74a
Ayane Satomi <chinodesuuu@gmail.com>
2021-08-19 13:06:55 +0800

    Finish up and tidy up Gpu Native classes
    Signed-off-by: Ayane Satomi <chinodesuuu@gmail.com>

---
+++ src/Akihabara/Gpu/GpuBufferFormat.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using Akihabara.Framework.ImageFormat;
+using Akihabara.Native.Gpu;
+
+namespace Akihabara.Gpu
+{
+    public enum GpuBufferFormat : UInt32
+    {
+        kUnknown = 0,
+        kBGRA32 = ('B' << 24) + ('G' << 16) + ('R' << 8) + ('A'),
+        kGrayFloat32 = ('L' << 24) + ('0' << 16) + ('0' << 8) + ('f'),
+        kGrayHalf16 = ('L' << 24) + ('0' << 16) + ('0' << 8) + ('h'),
+        kOneComponent8 = ('L' << 24) + ('0' << 16) + ('0' << 8) + ('8'),
+        kTwoComponentHalf16 = ('2' << 24) + ('C' << 16) + ('0' << 8) + ('h'),
+        kTwoComponentFloat32 = ('2' << 24) + ('C' << 16) + ('0' << 8) + ('f'),
+        kBiPlanar420YpCbCr8VideoRange = ('4' << 24) + ('2' << 16) + ('0' << 8) + ('v'),
+        kBiPlanar420YpCbCr8FullRange = ('4' << 24) + ('2' << 16) + ('0' << 8) + ('f'),
+        kRGB24 = 0x00000018, // Note: prefer BGRA32 whenever possible.
+        kRGBAHalf64 = ('R' << 24) + ('G' << 16) + ('h' << 8) + ('A'),
+        kRGBAFloat128 = ('R' << 24) + ('G' << 16) + ('f' << 8) + ('A'),
+    }
+
+    public static class GpuBufferFormatExtension
+    {
+        public static ImageFormat.Format ImageFormatFor(this GpuBufferFormat gpuBufferFormat)
+        {
+            return SafeNativeMethods.mp__ImageFormatForGpuBufferFormat__ui(gpuBufferFormat);
+        }
+
+        public static GlTextureInfo GlTextureInfoFor(this GpuBufferFormat gpuBufferFormat, int plane,
+            GlVersion glVersion = GlVersion.kGLES3)
+        {
+            UnsafeNativeMethods.mp__GlTextureInfoForGpuBufferFormat__ui_i_ui(gpuBufferFormat, plane, glVersion,
+                out var glTextureInfoPtr);
+            var glTextureInfo = Marshal.PtrToStructure<GlTextureInfo>(glTextureInfoPtr);
+            UnsafeNativeMethods.mp_GlTextureInfo__delete(glTextureInfoPtr);
+
+            return glTextureInfo;
+        }
+    }
+}
