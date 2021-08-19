e108e75bc3f6f978190c4293674b4bf15e761165
Ayane Satomi <chinodesuuu@gmail.com>
2021-08-19 12:54:16 +0800

    Add unported version of ResourceUtil
    Signed-off-by: Ayane Satomi <chinodesuuu@gmail.com>

---
+++ src/Akihabara/Native/Util/SafeResourceUtil.cs
@@ -0,0 +1,17 @@
+﻿using System.Resources;
+using System.Runtime.InteropServices;
+
+namespace Akihabara.Native.Util
+{
+    public partial class SafeNativeMethods : NativeMethods
+    {
+        //TODO: Change this to .NET native! This is a Unity API!
+        // https://docs.unity3d.com/Packages/com.unity.resourcemanager@1.6/manual/index.html
+        public static extern void mp__SetCustomGlobalResourceProvider__P(
+            [MarshalAs(UnmanagedType.FunctionPtr)]ResourceManager.ResourceProvider provider);
+
+        [DllImport (MediaPipeLibrary, ExactSpelling = true)]
+        public static extern void mp__SetCustomGlobalPathResolver__P(
+            [MarshalAs(UnmanagedType.FunctionPtr)]ResourceManager.PathResolver resolver);        
+    }
+}
