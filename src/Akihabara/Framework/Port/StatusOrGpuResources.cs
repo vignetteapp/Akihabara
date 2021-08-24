a003a359db19a1753e4dbdbfe8001daba1bac7a8
Ayane Satomi <chinodesuuu@gmail.com>
2021-08-25 04:04:21 +0800

    port GLContext and GpuResources
    Signed-off-by: Ayane Satomi <chinodesuuu@gmail.com>

---
+++ src/Akihabara/Framework/Port/StatusOrGpuResources.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Akihabara.Gpu;
+using Akihabara.Native;
+using SafeNativeMethods = Akihabara.Native.Gpu.SafeNativeMethods;
+using UnsafeNativeMethods = Akihabara.Native.Gpu.UnsafeNativeMethods;
+
+namespace Akihabara.Framework.Port
+{
+    public class StatusOrGpuResources : StatusOr<GpuResources>
+    {
+        public StatusOrGpuResources(IntPtr ptr) : base(ptr) {}
+
+        protected override void DeleteMpPtr()
+        {
+            UnsafeNativeMethods.mp_StatusOrGpuResources__delete(Ptr);
+        }
+
+        public override bool Ok => SafeNativeMethods.mp_StatusOrGpuBuffer__ok(MpPtr);
+
+        public override Status status
+        {
+            get
+            {
+                UnsafeNativeMethods.mp_StatusOrGpuResources__status(MpPtr, out var statusPtr).Assert();
+                
+                GC.KeepAlive(this);
+                return new Status(statusPtr);
+            }
+        }
+
+        public override GpuResources Value()
+        {
+            UnsafeNativeMethods.mp_StatusOrGpuResources__value(MpPtr, out var gpuResourcesPtr).Assert();
+            Dispose();
+
+            return new GpuResources(gpuResourcesPtr);
+        }
+    }
+}
---
+++ src/Akihabara/Gpu/GLContext.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Akihabara.Core;
+using Akihabara.Native;
+using SafeNativeMethods = Akihabara.Native.Gpu.SafeNativeMethods;
+using UnsafeNativeMethods = Akihabara.Native.Gpu.UnsafeNativeMethods;
+
+namespace Akihabara.Gpu
+{
+    public class GlContext : MpResourceHandle
+    {
+        private SharedPtrHandle _sharedPtrHandle;
+
+        public static GlContext GetCurrent()
+        {
+            UnsafeNativeMethods.mp_GlContext_GetCurrent(out var glContextPtr).Assert();
+
+            return glContextPtr == IntPtr.Zero ? null : new GlContext(glContextPtr);
+        }
+
+        public GlContext(IntPtr ptr, bool isOwner = true) : base(isOwner)
+        {
+            _sharedPtrHandle = new SharedPtr(ptr);
+            this.Ptr = _sharedPtrHandle.Get();
+        }
+
+        protected override void DisposeManaged()
+        {
+            if (_sharedPtrHandle != null)
+            {
+                _sharedPtrHandle.Dispose();
+                _sharedPtrHandle = null;
+            }
+            base.DisposeManaged();
+        }
+
+        protected override void DeleteMpPtr()
+        {
+            // this does nothing
+            // if this apparently gets called
+            // might as well donate my life savings to Kronii
+        }
+
+        public IntPtr SharedPtr => _sharedPtrHandle?.MpPtr ?? IntPtr.Zero;
+        
+        // GLES functions here!
+        // only use this if you're invoking this from a GLES device
+        public IntPtr EglDisplay => SafeNativeMethods.mp_GlContext__egl_display(MpPtr);
+
+        public IntPtr EglConfig => SafeNativeMethods.mp_GlContext__egl_config(MpPtr);
+
+        public IntPtr EglContext => SafeNativeMethods.mp_GlContext__egl_context(MpPtr);
+
+        public IntPtr NsglContext => SafeNativeMethods.mp_GlContext__nsgl_context(MpPtr);
+
+        public IntPtr EaglContext => SafeNativeMethods.mp_GlContext__eagl_context(MpPtr);
+        // end of GLES functions
+
+        public bool IsCurrent => SafeNativeMethods.mp_GlContext__IsCurrent(MpPtr);
+
+        public int GlMajorVersion => SafeNativeMethods.mp_GlContext__gl_major_version(MpPtr);
+
+        public int GlMinorVersion => SafeNativeMethods.mp_GlContext__gl_minor_version(MpPtr);
+
+        public long GlFinishCount => SafeNativeMethods.mp_GlContext__gl_finish_count(MpPtr);
+    }
+}
