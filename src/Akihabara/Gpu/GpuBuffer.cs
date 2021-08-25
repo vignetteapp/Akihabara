d974b14b939a03e83d5344160850b702bc8884d4
Ayane Satomi <chinodesuuu@gmail.com>
2021-08-25 12:23:43 +0800

    Add GpuBuffer implementation
    Signed-off-by: Ayane Satomi <chinodesuuu@gmail.com>

---
+++ src/Akihabara/Gpu/GpuBuffer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Akihabara.Core;
+using Akihabara.Native;
+using SafeNativeMethods = Akihabara.Native.Gpu.SafeNativeMethods;
+using UnsafeNativeMethods = Akihabara.Native.Gpu.UnsafeNativeMethods;
+
+namespace Akihabara.Gpu
+{
+    public class GpuBuffer : MpResourceHandle
+    {
+        public GpuBuffer(IntPtr ptr, bool isOwner = true) : base(ptr, isOwner) {}
+
+        public GpuBuffer(GlTextureBuffer glTextureBuffer) : base()
+        {
+            UnsafeNativeMethods.mp_GpuBuffer__PSgtb(glTextureBuffer.SharedPtr, out var ptr).Assert();
+            glTextureBuffer.Dispose();
+
+            this.Ptr = ptr;
+        }
+
+        protected override void DeleteMpPtr() => UnsafeNativeMethods.mp_GpuBuffer__delete(Ptr);
+
+        public GlTextureBuffer GetGlTextureBuffer() =>
+            new GlTextureBuffer(SafeNativeMethods.mp_GpuBuffer__GetGlTextureBufferSharedPtr(MpPtr),
+                false);
+
+        public GpuBufferFormat Format() => SafeNativeMethods.mp_GpuBuffer__format(MpPtr);
+
+        public int Width() => SafeNativeMethods.mp_GpuBuffer__width(MpPtr);
+
+        public int Height() => SafeNativeMethods.mp_GpuBuffer__height(MpPtr);
+    }
+}
