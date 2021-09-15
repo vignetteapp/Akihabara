8df93e56ac64481a10637ef9f696ea6e2fc767de
Speykious <speykious@gmail.com>
2021-09-16 00:46:55 +0200

    "Fixed" errors in ClassificationListPacket.cs - at least it builds

--- src/Akihabara/Framework/Packet/ClassificationListPacket.cs
+++ src/Akihabara/Framework/Packet/ClassificationListPacket.cs
@@ -1,5 +1,5 @@
 using System;
-using Akihabara.External;
+using Akihabara.Framework.Protobuf;
 using Akihabara.Framework.Port;
 using Akihabara.Native;
 
@@ -15,7 +15,7 @@ namespace Akihabara.Framework.Packet
             UnsafeNativeMethods.mp_Packet__GetClassificationList(MpPtr, out var serializedProtoPtr).Assert();
             GC.KeepAlive(this);
 
-            var rect = Protobuf.DeserializeProto<ClassificationList>(serializedProtoPtr, ClassificationList.Parser);
+            var rect = External.Protobuf.DeserializeProto<ClassificationList>(serializedProtoPtr, ClassificationList.Parser);
             UnsafeNativeMethods.mp_api_SerializedProto__delete(serializedProtoPtr);
 
             return rect;
@@ -23,6 +23,20 @@ namespace Akihabara.Framework.Packet
 
         public override StatusOr<ClassificationList> Consume()
         {
+            throw new NotSupportedException();
+        }
+
+        public override Status ValidateAsType()
+        {
+            /*
+            // This won't work as ValidateAsClassificationList doesn't exist as a C binding.
+            // However, I suspect that it exists somewhere in Mediapipe, and if so, we can bind it ourselves.
+            UnsafeNativeMethods.mp_Packet__ValidateAsClassificationList(MpPtr, out var statusPtr).Assert();
+
+            GC.KeepAlive(this);
+            return new Status(statusPtr);
+            */
+
             throw new NotSupportedException();
         }
     }
