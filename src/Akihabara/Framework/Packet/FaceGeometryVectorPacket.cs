dfac3568cd75788d5637b44e0b98cff9c59de6fb
Speykious <speykious@gmail.com>
2021-09-16 23:28:12 +0200

    Added 3 more packets

---
+++ src/Akihabara/Framework/Packet/FaceGeometryVectorPacket.cs
@@ -0,0 +1,35 @@
+﻿using Akihabara.Framework.Port;
+using Akihabara.Framework.Protobuf.FaceGeometry;
+using Akihabara.Native;
+using System;
+using System.Collections.Generic;
+
+namespace Akihabara.Framework.Packet
+{
+    public class FaceGeometryVectorPacket : Packet<List<FaceGeometry>>
+    {
+        public FaceGeometryVectorPacket() : base() { }
+        public FaceGeometryVectorPacket(IntPtr ptr, bool isOwner = true) : base(ptr, isOwner) { }
+
+        public override List<FaceGeometry> Get()
+        {
+            UnsafeNativeMethods.mp_Packet__GetFaceGeometryVector(MpPtr, out var serializedProtoVectorPtr).Assert();
+            GC.KeepAlive(this);
+
+            var geometries = External.Protobuf.DeserializeProtoVector<FaceGeometry>(serializedProtoVectorPtr, FaceGeometry.Parser);
+            UnsafeNativeMethods.mp_api_SerializedProtoVector__delete(serializedProtoVectorPtr);
+
+            return geometries;
+        }
+
+        public override StatusOr<List<FaceGeometry>> Consume()
+        {
+            throw new NotSupportedException();
+        }
+
+        public override Status ValidateAsType()
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
