8919339e461bfec397f7bd6927cb74be514e8e22
Speykious <speykious@gmail.com>
2021-09-16 23:28:12 +0200

    Added 3 more packets

---
+++ src/Akihabara/Framework/Packet/FaceGeometryPacket.cs
@@ -0,0 +1,34 @@
+﻿using Akihabara.Framework.Port;
+using Akihabara.Framework.Protobuf.FaceGeometry;
+using Akihabara.Native;
+using System;
+
+namespace Akihabara.Framework.Packet
+{
+    public class FaceGeometryPacket : Packet<FaceGeometry>
+    {
+        public FaceGeometryPacket() : base() { }
+        public FaceGeometryPacket(IntPtr ptr, bool isOwner = true) : base(ptr, isOwner) { }
+
+        public override FaceGeometry Get()
+        {
+            UnsafeNativeMethods.mp_Packet__GetFaceGeometry(MpPtr, out var serializedProtoPtr).Assert();
+            GC.KeepAlive(this);
+
+            var geometry = External.Protobuf.DeserializeProto<FaceGeometry>(serializedProtoPtr, FaceGeometry.Parser);
+            UnsafeNativeMethods.mp_api_SerializedProto__delete(serializedProtoPtr);
+
+            return geometry;
+        }
+
+        public override StatusOr<FaceGeometry> Consume()
+        {
+            throw new NotSupportedException();
+        }
+
+        public override Status ValidateAsType()
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
---
+++ src/Akihabara/Framework/Packet/TimedModelMatrixProtoListPacket.cs
@@ -0,0 +1,34 @@
+﻿using Akihabara.Framework.Port;
+using Akihabara.Framework.Protobuf;
+using Akihabara.Native;
+using System;
+
+namespace Akihabara.Framework.Packet
+{
+    public class TimedModelMatrixProtoListPacket : Packet<TimedModelMatrixProtoList>
+    {
+        public TimedModelMatrixProtoListPacket() : base() { }
+        public TimedModelMatrixProtoListPacket(IntPtr ptr, bool isOwner = true) : base(ptr, isOwner) { }
+
+        public override TimedModelMatrixProtoList Get()
+        {
+            UnsafeNativeMethods.mp_Packet__GetTimedModelMatrixProtoList(MpPtr, out var serializedProtoPtr).Assert();
+            GC.KeepAlive(this);
+
+            var matrixProtoList = External.Protobuf.DeserializeProto<TimedModelMatrixProtoList>(serializedProtoPtr, TimedModelMatrixProtoList.Parser);
+            UnsafeNativeMethods.mp_api_SerializedProto__delete(serializedProtoPtr);
+
+            return matrixProtoList;
+        }
+
+        public override StatusOr<TimedModelMatrixProtoList> Consume()
+        {
+            throw new NotSupportedException();
+        }
+
+        public override Status ValidateAsType()
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
