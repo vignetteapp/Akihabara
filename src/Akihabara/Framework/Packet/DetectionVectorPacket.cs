caeb693bf9520d9e8b5f7ce0fd868b2448011e52
Adryzz <46694241+adryzz@users.noreply.github.com>
2021-09-16 16:39:39 +0200

    Add most remaining packet types

---
+++ src/Akihabara/Framework/Packet/DetectionVectorPacket.cs
@@ -0,0 +1,39 @@
+﻿using Akihabara.External;
+using Akihabara.Framework.Port;
+using Akihabara.Framework.Protobuf;
+using Akihabara.Native;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Akihabara.Framework.Packet
+{
+    public class DetectionVectorPacket : Packet<List<Detection>>
+    {
+        public DetectionVectorPacket() : base() { }
+        public DetectionVectorPacket(IntPtr ptr, bool isOwner = true) : base(ptr, isOwner) { }
+
+        public override List<Detection> Get()
+        {
+            UnsafeNativeMethods.mp_Packet__GetDetectionVector(MpPtr, out var serializedProtoVectorPtr).Assert();
+            GC.KeepAlive(this);
+
+            var detections = External.Protobuf.DeserializeProtoVector<Detection>(serializedProtoVectorPtr, Detection.Parser);
+            UnsafeNativeMethods.mp_api_SerializedProtoVector__delete(serializedProtoVectorPtr);
+
+            return detections;
+        }
+
+        public override StatusOr<List<Detection>> Consume()
+        {
+            throw new NotSupportedException();
+        }
+
+        public override Status ValidateAsType()
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
---
+++ src/Akihabara/Framework/Packet/NormalizedRectPacket.cs
@@ -0,0 +1,39 @@
+﻿using Akihabara.External;
+using Akihabara.Framework.Port;
+using Akihabara.Framework.Protobuf;
+using Akihabara.Native;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Akihabara.Framework.Packet
+{
+    public class NormalizedRectPacket : Packet<NormalizedRect>
+    {
+        public NormalizedRectPacket() : base() { }
+        public NormalizedRectPacket(IntPtr ptr, bool isOwner = true) : base(ptr, isOwner) { }
+
+        public override NormalizedRect Get()
+        {
+            UnsafeNativeMethods.mp_Packet__GetNormalizedRect(MpPtr, out var serializedProtoPtr).Assert();
+            GC.KeepAlive(this);
+
+            var normalizedRect = External.Protobuf.DeserializeProto<NormalizedRect>(serializedProtoPtr, NormalizedRect.Parser);
+            UnsafeNativeMethods.mp_api_SerializedProto__delete(serializedProtoPtr);
+
+            return normalizedRect;
+        }
+
+        public override StatusOr<NormalizedRect> Consume()
+        {
+            throw new NotSupportedException();
+        }
+
+        public override Status ValidateAsType()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
