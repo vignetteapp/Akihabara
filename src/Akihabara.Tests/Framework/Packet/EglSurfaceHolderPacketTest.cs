12ca183ef3dfc2d340c4bea69245098572962bb3
Adryzz <46694241+adryzz@users.noreply.github.com>
2021-09-17 00:58:00 +0200

    Add last packet and test
    OpenGL ES only

---
+++ src/Akihabara.Tests/Framework/Packet/EglSurfaceHolderPacketTest.cs
@@ -0,0 +1,92 @@
+﻿using Akihabara.Core;
+using Akihabara.Framework;
+using Akihabara.Framework.Packet;
+using Akihabara.Framework.Port;
+using Akihabara.Gpu;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Akihabara.Tests.Framework.Packet
+{
+#if OPENGL_ES
+    [TestFixture]
+    public class EglSurfaceHolderPacketTest
+    {
+#region Constructor
+        [Test]
+        public void Ctor_ShouldInstantiatePacket_When_CalledWithNoArguments()
+        {
+            var packet = new EglSurfaceHolderPacket();
+
+            Assert.AreEqual(packet.ValidateAsType().Code, Status.StatusCode.Internal);
+        }
+
+        [Test]
+        public void Ctor_ShouldInstantiatePacket_When_CalledWithValue()
+        {
+            var eglSurfaceHolder = new EglSurfaceHolder();
+            var packet = new EglSurfaceHolderPacket(eglSurfaceHolder);
+
+            Assert.True(eglSurfaceHolder.IsDisposed);
+            Assert.True(packet.ValidateAsType().ok);
+            Assert.AreEqual(packet.Timestamp(), Timestamp.Unset());
+        }
+#endregion
+
+#region #isDisposed
+        [Test]
+        public void IsDisposed_ShouldReturnFalse_When_NotDisposedYet()
+        {
+            var packet = new EglSurfaceHolderPacket();
+
+            Assert.False(packet.IsDisposed);
+        }
+
+        [Test]
+        public void IsDisposed_ShouldReturnTrue_When_AlreadyDisposed()
+        {
+            var packet = new EglSurfaceHolderPacket();
+            packet.Dispose();
+
+            Assert.True(packet.IsDisposed);
+        }
+#endregion
+
+#region #Get
+        [Test] // previously [Test, SignalAbort] - I don't know why it was there
+        public void Get_ShouldThrowMediaPipeException_When_DataIsEmpty()
+        {
+            var packet = new EglSurfaceHolderPacket();
+
+            Assert.Throws<MediapipeException>(() => { packet.Get(); });
+        }
+
+        [Test]
+        public void Get_ShouldReturnEglSurfaceHolder_When_DataIsNotEmpty()
+        {
+            var eglSurfaceHolder = new EglSurfaceHolder();
+            eglSurfaceHolder.SetFlipY(true);
+            var packet = new EglSurfaceHolderPacket(eglSurfaceHolder);
+            var value = packet.Get();
+
+            Assert.False(value.OwnsResource());
+            Assert.True(value.FlipY());
+        }
+#endregion
+
+#region #DebugTypeName
+        [Test]
+        public void DebugTypeName_ShouldReturnFloat_When_ValueIsSet()
+        {
+            var packet = new EglSurfaceHolderPacket(new EglSurfaceHolder());
+
+            Assert.AreEqual(packet.DebugTypeName(), "std::unique_ptr<mediapipe::EglSurfaceHolder, std::default_delete<mediapipe::EglSurfaceHolder> >");
+        }
+#endregion
+    }
+#endif
+}
