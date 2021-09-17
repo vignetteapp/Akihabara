c7589e5ad4a1ac750ce01777cbcece847b0f3b5b
Speykious <speykious@gmail.com>
2021-09-17 19:18:53 +0200

    Ported Gpu/*.cs tests

---
+++ src/Akihabara.Tests/Gpu/GlContextTest.cs
@@ -0,0 +1,81 @@
+using Akihabara.Gpu;
+using Akihabara.Framework.Port;
+using NUnit.Framework;
+
+namespace Akihabara.Tests.Gpu
+{
+    public class GlContextTest
+    {
+        #region GetCurrent
+        [Test, GpuOnly]
+        public void GetCurrent_ShouldReturnNull_When_CalledOutOfGlContext()
+        {
+            var glContext = GlContext.GetCurrent();
+
+            Assert.Null(glContext);
+        }
+
+        [Test, GpuOnly]
+        public void GetCurrent_ShouldReturnCurrentContext_When_CalledInGlContext()
+        {
+            var glCalculatorHelper = new GlCalculatorHelper();
+            glCalculatorHelper.InitializeForTest(GpuResources.Create().Value());
+
+            glCalculatorHelper.RunInGlContext(() =>
+            {
+                var glContext = GlContext.GetCurrent();
+
+                Assert.NotNull(glContext);
+                Assert.True(glContext.IsCurrent);
+                return Status.Ok();
+            }).AssertOk();
+        }
+        #endregion
+
+        #region IsCurrent
+        public void IsCurrent_ShouldReturnFalse_When_CalledOutOfGlContext()
+        {
+            var glContext = GetGlContext();
+
+            Assert.False(glContext.IsCurrent);
+        }
+        #endregion
+
+        #region properties
+        [Test, GpuOnly]
+        public void ShouldReturnProperties()
+        {
+            var glContext = GetGlContext();
+
+            #if OPENGL_ES
+            Assert.AreNotEqual(glContext.eglDisplay, IntPtr.Zero);
+            Assert.AreNotEqual(glContext.eglConfig, IntPtr.Zero);
+            Assert.AreNotEqual(glContext.eglContext, IntPtr.Zero);
+            Assert.AreEqual(glContext.glMajorVersion, 3);
+            Assert.AreEqual(glContext.glMinorVersion, 2);
+            Assert.AreEqual(glContext.glFinishCount, 0);
+            #elif STANDALONE_OSX
+            Assert.AreNotEqual(glContext.nsglContext, IntPtr.Zero);
+            #elif IOS
+            Assert.AreNotEqual(glContext.eaglContext, IntPtr.Zero);
+            #endif
+        }
+        #endregion
+
+        private GlContext GetGlContext()
+        {
+            GlContext glContext = null;
+
+            var glCalculatorHelper = new GlCalculatorHelper();
+            glCalculatorHelper.InitializeForTest(GpuResources.Create().Value());
+
+            glCalculatorHelper.RunInGlContext(() =>
+            {
+                glContext = GlContext.GetCurrent();
+                return Status.Ok();
+            }).AssertOk();
+
+            return glContext;
+        }
+    }
+}
---
+++ src/Akihabara.Tests/Gpu/GpuResourcesTest.cs
@@ -0,0 +1,37 @@
+using Akihabara.Gpu;
+using NUnit.Framework;
+
+namespace Tests
+{
+    public class GpuResourcesTest
+    {
+        #region Create
+        [Test, GpuOnly]
+        public void Create_ShouldReturnStatusOrGpuResources()
+        {
+            var statusOrGpuResources = GpuResources.Create();
+
+            Assert.True(statusOrGpuResources.Ok);
+        }
+        #endregion
+
+        #region IsDisposed
+        [Test, GpuOnly]
+        public void IsDisposed_ShouldReturnFalse_When_NotDisposedYet()
+        {
+            var gpuResources = GpuResources.Create().Value();
+
+            Assert.False(gpuResources.IsDisposed);
+        }
+
+        [Test, GpuOnly]
+        public void IsDisposed_ShouldReturnTrue_When_AlreadyDisposed()
+        {
+            var gpuResources = GpuResources.Create().Value();
+            gpuResources.Dispose();
+
+            Assert.True(gpuResources.IsDisposed);
+        }
+        #endregion
+    }
+}
