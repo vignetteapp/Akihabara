43f946b3b8845d4144b1ca382a193b6693accd0c
Speykious <speykious@gmail.com>
2021-09-29 00:34:01 +0200

    Create HelloWorld and FaceMesh example projects

---
+++ examples/Akihabara.Examples.HelloWorld/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Akihabara.Examples.HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
