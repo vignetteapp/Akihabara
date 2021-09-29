82faaee3b9e085398a83b3da2e8ca75906d9f53a
Speykious <speykious@gmail.com>
2021-09-29 12:35:10 +0200

    Create OnRawIO example project

---
+++ examples/Akihabara.Examples.OnRawIO/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Akihabara.Examples.OnRawIO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
