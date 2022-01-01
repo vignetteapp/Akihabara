3344579680e97c88a3102f14822de64087d60bda
Ayane <ayane@vignetteapp.org>
2022-01-01 22:15:52 +0800

    remove "lib-" prefix in mediapipe_c
    This requires verification firsthand but please merge this if this was the main cause of the issue.

--- src/Akihabara/Native/NativeMethods.cs
+++ src/Akihabara/Native/NativeMethods.cs
@@ -13,6 +13,6 @@ namespace Akihabara.Native
         // variant we're basing off, which you can find here: https://git.io/Jc3y1
         //
         // However, since we're targeting PC for now, we'll go for mediapipe_c for now.
-        internal const string MediaPipeLibrary = "libmediapipe_c";
+        internal const string MediaPipeLibrary = "mediapipe_c";
     }
 }
